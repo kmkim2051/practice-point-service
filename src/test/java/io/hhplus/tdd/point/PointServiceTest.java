b67db3157891d6119df30f60a681fd7558eac6dc
km <qaws2051@gmail.com>
2025-10-23 01:28:29 +0900

    [Refactor] Nested 클래스 적용

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -4,6 +4,7 @@ import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -27,7 +28,22 @@ import static org.mockito.Mockito.*;
  *     - 개별적으로 3가지의 요구사항을 정의하고, 이를 구현함
  *         - ex) 포인트 출금은 1000원 이하로 할 수 없다
  *         - ex) 포인트 사용은 100원 단위로만 가능하다
+ *
+ *
+ * ### **`STEP1 - TDD 기본`**
+ * - /point 패키지(디렉토리) 내에 PointService 기본 기능 작성
+ * - /database 패키지의 구현체는 수정하지 않고, 이를 활용해 기능 구현
+ * - 각 기능에 대한 단위 테스트 작성
+ * - 총 4가지 기본 기능 구현 (포인트 조회, 충전, 사용, 내역 조회)
+ *
+ * ### **`STEP2 - TDD 심화`**
+ *
+ * - 포인트 충전, 사용에 대한 정책 추가 (잔고 부족, 최대 잔고 등)
+ * - 동일한 사용자에 대한 동시 요청이 정상적으로 처리될 수 있도록 개선
+ * - 주어진 4가지 기능에 대한 통합 테스트 작성
+ * - 선택한 언어에 대한 동시성 제어 방식 및 장/단점을 기술한 보고서 작성 (README.md)
  * */
+@DisplayName("PointService 단위 테스트")
 public class PointServiceTest {
 
     private UserPointTable userPointTable;
@@ -35,131 +51,159 @@ public class PointServiceTest {
     private PointService pointService;
 
     @BeforeEach
-    void setUpPointService() {
+    void setUp() {
         userPointTable = mock(UserPointTable.class);
         pointHistoryTable = mock(PointHistoryTable.class);
         pointService = new PointServiceImpl(userPointTable, pointHistoryTable);
     }
 
-    // ---------- 1. 포인트 조회 ----------
-    @Test
-    @DisplayName("사용자 포인트 조회 결과 현재 포인트가 조회된다")
-    void getUserPointIsNotNull() {
-        // given
-        long userId = 1L;
-        long currentAmount = 100L;
-        UserPoint mockUserPoint = new UserPoint(userId, currentAmount, System.currentTimeMillis());
+    @Nested
+    @DisplayName("포인트 조회")
+    class GetUserPoint {
 
-        when(userPointTable.selectById(userId))
-                .thenReturn(mockUserPoint);
+        @Test
+        @DisplayName("사용자의 현재 포인트 잔액을 조회할 수 있다")
+        void getUserPoint() {
+            // given
+            long userId = 1L;
+            long currentAmount = 100L;
+            UserPoint mockUserPoint = new UserPoint(userId, currentAmount, System.currentTimeMillis());
 
-        // when
-        UserPoint userPoint = pointService.getUserPoint(userId);
+            when(userPointTable.selectById(userId))
+                    .thenReturn(mockUserPoint);
 
-        // then
-        assertThat(userPoint).isNotNull();
-        assertThat(userPoint.id()).isEqualTo(userId);
-        assertThat(userPoint.point()).isEqualTo(currentAmount);
+            // when
+            UserPoint userPoint = pointService.getUserPoint(userId);
+
+            // then
+            assertThat(userPoint).isNotNull();
+            assertThat(userPoint.id()).isEqualTo(userId);
+            assertThat(userPoint.point()).isEqualTo(currentAmount);
+        }
     }
 
-    // ---------- 2. 충전 ----------
-    @Test
-    @DisplayName("처음 포인트를 충전하면 반환된 UserPoint 금액이 충전 금액과 동일하다")
-    void chargeUserPointIsNotNull() {
-        // given
-        long userId = 1L;
-        long amount = 500L;
-        UserPoint currentPoint = new UserPoint(userId, 0L, System.currentTimeMillis());
-        UserPoint chargedPoint = new UserPoint(userId, amount, System.currentTimeMillis());
+    @Nested
+    @DisplayName("포인트 충전")
+    class ChargeUserPoint {
 
-        when(userPointTable.selectById(userId))
-                .thenReturn(currentPoint);
-        when(userPointTable.insertOrUpdate(userId, amount))
-                .thenReturn(chargedPoint);
+        @Test
+        @DisplayName("포인트를 충전하면 충전 금액만큼 증가한다")
+        void chargePoint() {
+            // given
+            long userId = 1L;
+            long amount = 500L;
+            UserPoint currentPoint = new UserPoint(userId, 0L, System.currentTimeMillis());
+            UserPoint chargedPoint = new UserPoint(userId, amount, System.currentTimeMillis());
 
-        // when
-        UserPoint userPoint = pointService.chargeUserPoint(userId, amount);
+            when(userPointTable.selectById(userId))
+                    .thenReturn(currentPoint);
+            when(userPointTable.insertOrUpdate(userId, amount))
+                    .thenReturn(chargedPoint);
 
-        // then
-        assertThat(userPoint).isNotNull();
-        assertThat(userPoint.id()).isEqualTo(userId);
-        assertThat(userPoint.point()).isEqualTo(amount);
+            // when
+            UserPoint userPoint = pointService.chargeUserPoint(userId, amount);
+
+            // then
+            assertThat(userPoint).isNotNull();
+            assertThat(userPoint.id()).isEqualTo(userId);
+            assertThat(userPoint.point()).isEqualTo(amount);
+        }
     }
 
-    // ---------- 3. 포인트 사용 ----------
-    @Test
-    @DisplayName("포인트를 사용하면 사용 금액만큼 차감된다")
-    void usePoint() {
-        // given
-        long userId = 1L;
-        long currentAmount = 500L;
-        long useAmount = 100;
+    @Nested
+    @DisplayName("포인트 사용")
+    class UseUserPoint {
 
-        UserPoint currentPoint = new UserPoint(userId, currentAmount, System.currentTimeMillis());
-        UserPoint expectedPoint = new UserPoint(userId, currentAmount - useAmount, System.currentTimeMillis());
+        @Test
+        @DisplayName("포인트를 사용하면 사용 금액만큼 차감된다")
+        void usePoint() {
+            // given
+            long userId = 1L;
+            long currentAmount = 500L;
+            long useAmount = 100L;
 
-        when(userPointTable.selectById(userId))
-                .thenReturn(currentPoint);
-        when(userPointTable.insertOrUpdate(currentPoint.id(), currentAmount - useAmount))
-                .thenReturn(expectedPoint);
-        // when
-        UserPoint pointAfterUse = pointService.useUserPoint(userId, useAmount);
+            UserPoint currentPoint = new UserPoint(userId, currentAmount, System.currentTimeMillis());
+            UserPoint expectedPoint = new UserPoint(userId, currentAmount - useAmount, System.currentTimeMillis());
 
-        // then
-        assertThat(pointAfterUse.point()).isEqualTo(currentAmount - useAmount);
+            when(userPointTable.selectById(userId))
+                    .thenReturn(currentPoint);
+            when(userPointTable.insertOrUpdate(currentPoint.id(), currentAmount - useAmount))
+                    .thenReturn(expectedPoint);
+
+            // when
+            UserPoint pointAfterUse = pointService.useUserPoint(userId, useAmount);
+
+            // then
+            assertThat(pointAfterUse.point()).isEqualTo(currentAmount - useAmount);
+        }
+
+        @Test
+        @DisplayName("잔액이 부족하면 포인트 사용에 실패하고 예외가 발생한다")
+        void usePointWithInsufficientBalance() {
+            // given
+            long userId = 1L;
+            long currentAmount = 500L;
+            long useAmount = 501L;
+
+            UserPoint currentPoint = new UserPoint(userId, currentAmount, System.currentTimeMillis());
+            when(userPointTable.selectById(userId)).thenReturn(currentPoint);
+
+            // when & then
+            assertThatThrownBy(() -> pointService.useUserPoint(userId, useAmount))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessageContaining("잔액이 부족합니다");
+
+            verify(userPointTable, never()).insertOrUpdate(anyLong(), anyLong());
+        }
     }
 
-    @Test
-    @DisplayName("현재 잔액 이상의 포인트를 사용하면 예외를 던진다.")
-    void useTooMuchPointThrowsException() {
-        // given
-        long userId = 1L;
-        long amount = 500L;
-        long useAmount = 501L;
+    @Nested
+    @DisplayName("포인트 내역 조회")
+    class GetUserPointHistories {
 
-        UserPoint currentPoint = new UserPoint(userId, amount, System.currentTimeMillis());
-        when(userPointTable.selectById(userId)).thenReturn(currentPoint);
+        @Test
+        @DisplayName("사용자의 포인트 충전/사용 내역을 조회할 수 있다")
+        void getPointHistories() {
+            // given
+            long userId = 1L;
+            long pointHistoryId1 = 1L;
+            long chargeAmount = 500L;
+            long pointHistoryId2 = 2L;
+            long useAmount = 100L;
 
-        // when, then
-        assertThatThrownBy(() -> pointService.useUserPoint(userId, useAmount))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("잔액이 부족합니다");
+            PointHistory chargeHistory = new PointHistory(
+                    pointHistoryId1,
+                    userId,
+                    chargeAmount,
+                    TransactionType.CHARGE,
+                    System.currentTimeMillis()
+            );
+            PointHistory useHistory = new PointHistory(
+                    pointHistoryId2,
+                    userId,
+                    useAmount,
+                    TransactionType.USE,
+                    System.currentTimeMillis()
+            );
 
-        verify(userPointTable, never()).insertOrUpdate(anyLong(), anyLong());
-    }
-    
-    @Test
-    @DisplayName("사용자의 포인트 이용 내역을 조회할 수 있다")
-    void selectPointHistory() {
-        // given
-        long userId = 1L;
+            List<PointHistory> mockHistories = List.of(chargeHistory, useHistory);
 
-        long pointHistoryId1 = 1L;
-        long amount2 = 100L;
+            when(pointHistoryTable.selectAllByUserId(userId))
+                    .thenReturn(mockHistories);
 
-        long pointHistoryId2 = 2L;
-        long amount1 = 500L;
+            // when
+            List<PointHistory> userPointHistories = pointService.getUserPointHistories(userId);
 
-        PointHistory chargeHistory = new PointHistory(pointHistoryId1, userId, amount1, TransactionType.CHARGE, System.currentTimeMillis());
-        PointHistory useHistory = new PointHistory(pointHistoryId2, userId, amount2, TransactionType.USE, System.currentTimeMillis());
+            // then
+            assertThat(userPointHistories).hasSize(2);
 
-        List<PointHistory> mockHistories = List.of(chargeHistory, useHistory);
+            assertThat(userPointHistories.get(0).id()).isEqualTo(pointHistoryId1);
+            assertThat(userPointHistories.get(0).userId()).isEqualTo(userId);
+            assertThat(userPointHistories.get(0).amount()).isEqualTo(chargeAmount);
+            assertThat(userPointHistories.get(0).type()).isEqualTo(TransactionType.CHARGE);
 
-        when(pointHistoryTable.selectAllByUserId(userId))
-                .thenReturn(mockHistories);
-
-        // when
-        List<PointHistory> userPointHistories = pointService.getUserPointHistories(userId);
-
-        // then
-        assertThat(userPointHistories).hasSize(2);
-
-        assertThat(userPointHistories.get(0).id()).isEqualTo(pointHistoryId1);
-        assertThat(userPointHistories.get(0).userId()).isEqualTo(userId);
-        assertThat(userPointHistories.get(0).amount()).isEqualTo(amount1);
-        assertThat(userPointHistories.get(0).type()).isEqualTo(TransactionType.CHARGE);
-
-        assertThat(userPointHistories.get(1).id()).isEqualTo(pointHistoryId2);
-        assertThat(userPointHistories.get(1).type()).isEqualTo(TransactionType.USE);
+            assertThat(userPointHistories.get(1).id()).isEqualTo(pointHistoryId2);
+            assertThat(userPointHistories.get(1).type()).isEqualTo(TransactionType.USE);
+        }
     }
 }

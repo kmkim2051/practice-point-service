789265ddfb419b6ed9393cdb16170326237625c2
km <qaws2051@gmail.com>
2025-10-24 00:07:10 +0900

    [Feat] 충전, 사용 커스텀 예외 핸들러 추가

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd;
 
+import io.hhplus.tdd.exception.PointException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,6 +8,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = PointException.InvalidChargeAmountException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidChargeAmountException(PointException.InvalidChargeAmountException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ExceptionHandler(value = PointException.InvalidUseAmountException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidUseAmountException(PointException.InvalidUseAmountException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ExceptionHandler(value = PointException.InsufficientPointException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientPointException(PointException.InsufficientPointException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
